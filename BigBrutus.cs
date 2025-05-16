97157476d9f70eaeaa025ab239caabe1cca1c428
timj41414 <trodgers6400@gmail.com>
2025-05-15 20:51:03 -0700

    Add files via upload
    Practicing Numerical Data Types by making a basic display sheet for a fictional company named "Big Brutus"
    Completed 15 May 2025

---
+++ BigBrutus.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace Numbers
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // Number of pizza shops
+      int pizzaShops = 4332;
+
+      // Number of employees
+      int totalEmployees = 86928;
+
+      // Revenue
+      decimal revenue = 390819.28m;
+
+      // Log the values to the console:
+      Console.WriteLine("Amount of Big Brutus locations is: " + pizzaShops);
+      Console.WriteLine("Total number of employees working at Big Brutus pizzarias is: " + totalEmployees);
+     Console.WriteLine("The average income of one Big Brutus pizzaria is: " + revenue);
+   	}
+  }
+}
